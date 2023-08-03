93d38f1d2c3a46a049867d3c5b7972d97c00afdc
moononournation <moononournation@gmail.com>
2023-08-03 22:37:06 +0800

    add LvglBenchmark_VGA

---
+++ LvglBenchmark_VGA/LvglBenchmark_VGA.ino
@@ -0,0 +1,198 @@
+/*******************************************************************************
+ * LVGL Benchmark
+ * This is a benchmark demo for LVGL - Light and Versatile Graphics Library
+ * import from: https://github.com/lvgl/lv_demos.git
+ *
+ * Dependent libraries:
+ * LVGL: https://github.com/lvgl/lvgl.git
+ *
+ * LVGL Configuration file:
+ * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
+ * to your_arduino_path/libraries/lv_conf.h
+ *
+ * In lv_conf.h around line 15, enable config file:
+ * #if 1 // Set it to "1" to enable content
+ *
+ * Then find and set:
+ * #define LV_COLOR_DEPTH     16
+ * #define LV_TICK_CUSTOM     1
+ *
+ * #define LV_COLOR_16_SWAP   0 // for Canvas Class
+ *
+ * Enable LVGL Demo Benchmark:
+ * #define LV_USE_DEMO_BENCHMARK 1
+ *
+ * Enables support for compressed fonts:
+ * #define LV_USE_FONT_COMPRESSED 1
+ *
+ * Customize font size:
+ * #define LV_FONT_MONTSERRAT_12 1
+ * #define LV_FONT_DEFAULT &lv_font_montserrat_12
+ ******************************************************************************/
+#include "lv_demo_benchmark.h"
+
+#include "TDECK_PINS.h"
+
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+ *
+ * Arduino_GFX try to find the settings depends on selected board in Arduino IDE
+ * Or you can define the display dev kit not in the board list
+ * Defalult pin list for non display dev kit:
+ * Arduino Nano, Micro and more: CS:  9, DC:  8, RST:  7, BL:  6, SCK: 13, MOSI: 11, MISO: 12
+ * ESP32 various dev board     : CS:  5, DC: 27, RST: 33, BL: 22, SCK: 18, MOSI: 23, MISO: nil
+ * ESP32-C3 various dev board  : CS:  7, DC:  2, RST:  1, BL:  3, SCK:  4, MOSI:  6, MISO: nil
+ * ESP32-S2 various dev board  : CS: 34, DC: 38, RST: 33, BL: 21, SCK: 36, MOSI: 35, MISO: nil
+ * ESP32-S3 various dev board  : CS: 40, DC: 41, RST: 42, BL: 48, SCK: 36, MOSI: 35, MISO: nil
+ * ESP8266 various dev board   : CS: 15, DC:  4, RST:  2, BL:  5, SCK: 14, MOSI: 13, MISO: 12
+ * Raspberry Pi Pico dev board : CS: 17, DC: 27, RST: 26, BL: 28, SCK: 18, MOSI: 19, MISO: 16
+ * RTL8720 BW16 old patch core : CS: 18, DC: 17, RST:  2, BL: 23, SCK: 19, MOSI: 21, MISO: 20
+ * RTL8720_BW16 Official core  : CS:  9, DC:  8, RST:  6, BL:  3, SCK: 10, MOSI: 12, MISO: 11
+ * RTL8722 dev board           : CS: 18, DC: 17, RST: 22, BL: 23, SCK: 13, MOSI: 11, MISO: 12
+ * RTL8722_mini dev board      : CS: 12, DC: 14, RST: 15, BL: 13, SCK: 11, MOSI:  9, MISO: 10
+ * Seeeduino XIAO dev board    : CS:  3, DC:  2, RST:  1, BL:  0, SCK:  8, MOSI: 10, MISO:  9
+ * Teensy 4.1 dev board        : CS: 39, DC: 41, RST: 40, BL: 22, SCK: 13, MOSI: 11, MISO: 12
+ ******************************************************************************/
+#include <Arduino_GFX_Library.h>
+#define GFX_DEV_DEVICE LILYGO_T_DECK
+#define GFX_EXTRA_PRE_INIT()                 \
+  {                                          \
+    pinMode(TDECK_TFT_BACKLIGHT, OUTPUT);    \
+    digitalWrite(TDECK_TFT_BACKLIGHT, HIGH); \
+  }
+Arduino_DataBus *bus = new Arduino_ESP32SPI(TDECK_TFT_DC, TDECK_TFT_CS, TDECK_SPI_SCK, TDECK_SPI_MOSI, TDECK_SPI_MISO);
+Arduino_GFX *gfx = new Arduino_ST7789(bus, GFX_NOT_DEFINED /* RST */, 1 /* rotation */, false /* IPS */);
+Arduino_Canvas *canvasGfx = new Arduino_Canvas(640 /* width */, 480 /* height */, gfx);
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Please config the touch panel in touch.h
+ ******************************************************************************/
+#include "touch.h"
+
+/* Change to your screen resolution */
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+static unsigned long last_ms;
+
+/* Display flushing */
+static bool canvasUpdated = false;
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  canvasGfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  canvasGfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  canvasUpdated = true;
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // while(!Serial);
+  Serial.println("LVGL Benchmark Demo");
+
+#ifdef GFX_EXTRA_PRE_INIT
+  GFX_EXTRA_PRE_INIT();
+#endif
+
+  // Init Display
+  if (!canvasGfx->begin(80000000))
+  {
+    Serial.println("gfx->begin() failed!");
+  }
+  canvasGfx->fillScreen(BLACK);
+
+#ifdef GFX_BL
+  pinMode(GFX_BL, OUTPUT);
+  digitalWrite(GFX_BL, HIGH);
+#endif
+
+  // Init touch device
+  touch_init(canvasGfx->width(), canvasGfx->height(), canvasGfx->getRotation());
+
+  lv_init();
+
+  screenWidth = canvasGfx->width();
+  screenHeight = canvasGfx->height();
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 40, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * 40);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * 40);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    lv_demo_benchmark();
+
+    Serial.println("Setup done");
+  }
+  last_ms = millis();
+}
+
+void loop()
+{
+  lv_timer_handler(); /* let the GUI do its work */
+  if (canvasUpdated)
+  {
+    canvasGfx->flushQuad();
+    canvasUpdated = false;
+  }
+  delay(5);
+}
